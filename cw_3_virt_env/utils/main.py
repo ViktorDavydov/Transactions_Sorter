6fb0f0223a45d2ab2bf8f11387eef00e91e9d2c9
ViktorDavydov <davydov.viktor.andreevich@gmail.com>
2023-08-30 23:12:10 +0300

    main.py dockstring fix

--- cw_3_virt_env/utils/main.py
+++ cw_3_virt_env/utils/main.py
@@ -1,9 +1,12 @@
+# Making imports
 from cw_3_virt_env.utils.utils import (json_load, sort_last_five_list,
                                        final_information)
 
+# Assigning variables
 all_operation_list = json_load("../../operations.json")
 last_five_sorted = sort_last_five_list(all_operation_list)
 final_information_list = final_information(last_five_sorted)
 
+# Printing last five formed operations
 for operation in final_information_list:
     print(operation)
